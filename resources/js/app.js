0734391492ab10073eec2008a9db7c460ce44236
Roshan <rcroshan63@gmail.com>
2017-06-22 00:01:20 +0545

    printed random grid id

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,50 @@
-var boardGrid = [
-                    ["E", "E", "E"],
-                    ["E", "E", "E"],
-                    ["E", "E", "E"]
-                ];
+var boardGrid = ["E", "E", "E", "E", "E", "E", "E", "E", "E"];
+
+var running = false;
 
 $(document).ready(function() {
-    $(".tic").click(function(){
+
+    $(".tic").click(function() {
+        running = true;
         var gridId = $(this).data("grid-id");
 
-        
+        boardGrid[gridId] = "X";
+        $(this).addClass("belize-hole");
+
+        var available = findAvailable(boardGrid);
+
+        var randomGridId = getRandomGridId(available);
+
+        boardGrid[randomGridId] = "O";
+
+        var selector = "[data-grid-id= " + randomGridId + "]";
+
+        $(selector).addClass("pumpkin");
     });
 });
+
+function findAvailable(board) {
+    var available = new Array();
+    var avl = new Array();
+
+    for(var i=1; i<=9; i++) {
+        if (board[i] == "E") {
+            available.push(i);
+        }
+    }
+
+    return available;
+}
+
+function getRandomGridId(available)
+{
+    // [TODO what if array is empty]
+
+    if (available.length) {
+        var random = available[Math.floor(Math.random() * available.length)];
+
+        return random;
+    }
+
+    return;
+}
